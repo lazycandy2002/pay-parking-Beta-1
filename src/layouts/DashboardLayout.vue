82319bc1a8d45cef091f2d347144bc463571faa6
Jared Gamutin <90660669+lazycandy2002@users.noreply.github.com>
2025-05-17 11:43:01 +0800

    Change Headers

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -1,4 +1,3 @@
-
 <style scoped>
     .layout{
         border: 1px solid #d7dde4;
@@ -7,90 +6,74 @@
         border-radius: 4px;
         overflow: hidden;
     }
-    .layout .ivu-menu{
-        z-index: 0
+    .layout-header{
+        background: #2d8cf0;
+        padding: 0;
+        height: auto;
     }
     .layout-header-bar{
         background: #fff;
         box-shadow: 0 1px 1px rgba(0,0,0,.1);
+        display: flex;
+        align-items: center;
+        height: 50px;
+        padding-left: 20px;
     }
-    .layout-logo-left{
-        width: 90%;
-        height: 30px;
-        background: #5b6270;
-        border-radius: 3px;
-        margin: 15px auto;
-    }
-    .menu-icon{
-        transition: all .3s;
-    }
-    .rotate-icon{
-        transform: rotate(-90deg);
-    }
-    .menu-item span{
-        display: inline-block;
-        overflow: hidden;
-        min-width: 69px;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        vertical-align: bottom;
-        transition: width .2s ease .2s;
+    .layout-logo {
+    width: 100px;
+    height:100px;
+    background: url('./src/assets/J&W Logo.png') no-repeat center center;
+    background-size: contain;
+    border-radius: 3px;
+    margin: 10px 20px;
+    float: left;
+}
+    .menu-item{
+        display: flex;
+        align-items: center;
     }
     .menu-item i{
-        transform: translateX(0px);
-        transition: font-size .2s ease, transform .2s ease;
-        vertical-align: middle;
-        font-size: 16px;
-    }
-    .collapsed-menu span{
-        width: 0px;
-        transition: width .2s ease;
-    }
-    .collapsed-menu i{
-        transform: translateX(5px);
-        transition: font-size .2s ease .2s, transform .2s ease .2s;
-        vertical-align: middle;
-        font-size: 22px;
+        margin-right: 5px;
     }
 </style>
 <template>
     <div class="layout">
         <Layout style="height: 100vh;">
-            <Sider ref="side1" hide-trigger collapsible :collapsed-width="78" v-model="isCollapsed">
-                <Menu :active-name="currentRoute" theme="dark" width="auto" :class="menuitemClasses">
-                    <MenuItem @click="goTo('/home')" name="/home">
+            <Header class="layout-header">
+                <Menu mode="horizontal" theme="dark" :active-name="currentRoute">
+                    <div class="layout-logo"></div>
+                    <MenuItem @click.native="goTo('/home')" name="/home">
                         <Icon type="ios-home"></Icon>
                         <span>Dashboard</span>
                     </MenuItem>
-                    <MenuItem @click="goTo('/new-registration')" name="/new-registration">
+                    <MenuItem @click.native="goTo('/new-registration')" name="/new-registration">
                         <Icon type="ios-person-add"></Icon>
                         <span>New Registration</span>
                     </MenuItem>
-                    <MenuItem @click="goTo('/transactions')" name="/transactions">
+                    <MenuItem @click.native="goTo('/transactions')" name="/transactions">
                         <Icon type="ios-card"></Icon>
                         <span>Transaction</span>
                     </MenuItem>
-                    <MenuItem @click="goTo('/parking-records')" name="/parking-records">
+                    <MenuItem @click.native="goTo('/parking-records')" name="/parking-records">
                         <Icon type="ios-list"></Icon>
                         <span>Parking Records</span>
                     </MenuItem>
-                    <MenuItem @click="goTo('/parking-slots')" name="/parking-slots">
+                    <MenuItem @click.native="goTo('/parking-slots')" name="/parking-slots">
                         <Icon type="ios-car"></Icon>
                         <span>Parking Slots</span>
                     </MenuItem>
-                    <MenuItem @click="goTo('/settings')" name="/settings">
+                    <MenuItem @click.native="goTo('/settings')" name="/settings">
                         <Icon type="ios-settings"></Icon>
                         <span>Settings</span>
                     </MenuItem>
-                    <MenuItem @click="goTo('/about')" name="/help">
+                    <MenuItem @click.native="goTo('/about')" name="/help">
                         <Icon type="ios-information-circle"></Icon>
                         <span>About</span>
                     </MenuItem>
                 </Menu>
-            </Sider>
+            </Header>
             <Layout>
-                <Header :style="{padding: 0}" class="layout-header-bar">
-                    <Icon @click="collapsedSider" :class="rotateIcon" :style="{margin: '0 20px'}" type="md-menu" size="24"></Icon>
+                <Header class="layout-header-bar">
                     <b>J&W PayParking Monthly Overview</b>
                 </Header>
                 <Content :style="{margin: '20px', background: '#fff', minHeight: '260px', overflowY: 'auto'}">
@@ -104,31 +87,15 @@
     export default {
         data () {
             return {
-                isCollapsed: false
+                // No need for isCollapsed in horizontal layout
             }
         },
         computed: {
-            rotateIcon () {
-                return [
-                    'menu-icon',
-                    this.isCollapsed ? 'rotate-icon' : ''
-                ];
-            },
-            menuitemClasses () {
-                return [
-                    'menu-item',
-                    this.isCollapsed ? 'collapsed-menu' : ''
-                ]
-            },
-
             currentRoute() {
                 return this.$route.path
             }
         },
         methods: {
-            collapsedSider () {
-                this.$refs.side1.toggleCollapse();
-            },
             goTo(path) {
                 this.$router.push(path);
             }
